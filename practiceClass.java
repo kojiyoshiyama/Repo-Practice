bb7a7af289323792fad9d3bef0a10c786fee4fe0
kyoshiyama <kojiyo@uw.edu>
2023-10-24 12:18:17 -0700

    commit practice

--- practiceClass.java
+++ practiceClass.java
@@ -1,2 +1,9 @@
 public class practiceClass {
+    public void printSomething() {
+        System.out.println("This is my new branch");
+        System.out.println("This is my new branch");
+        System.out.println("This is my new branch");
+
+
+    }
 }
